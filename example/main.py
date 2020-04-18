5d5792d8bbe4bc9c6fe2a3a90578817d35113bb9
Dohyun Park <dhlife09@gmail.com>
2020-04-19 02:09:06 +0900

    Create main.py

---
+++ example/main.py
@@ -0,0 +1,67 @@
+import requests
+import json
+import re #계산을 위한 특수문자 제거
+
+#####
+korea = "http://api.corona-19.kr/korea?serviceKey="
+country = "http://api.corona-19.kr/korea/country?serviceKey="
+key = "여기에 API 키를 입력해주세요." #API 키(https://api.corona-19.kr/ 에서 무료 발급 가능)
+###
+
+response = requests.get(korea + key)
+text = response.text
+data = json.loads(text)
+
+response2 = requests.get(country + key)
+text2 = response2.text
+data2 = json.loads(text2)
+
+#####
+code = response.status_code
+code2 = response2.status_code
+
+if code == 200: #API 응답 헤더 코드가 정상(200)일 경우 데이터 제공
+  if code2 == 200:
+    print('[',(data["updateTime"]),']')
+    print("\n")
+
+    print('확진자:',data["TotalCase"],' [+',data2["data0_1"],']')
+    print('완치자:',data["TotalRecovered"],' [+',data["TodayRecovered"],']')
+    print('사망자:',data["TotalDeath"])
+    print('치료중:',data["NowCase"])
+    print("\n")
+
+    a = int(re.sub('[-=.,#/?:$}]', '', data["caseCount"]))
+    b = int(re.sub('[-=.,#/?:$}]', '', data["TotalChecking"]))
+    
+    caseper = a / b * 100 #확진율계산법: 양성/검사완료수 * 100
+
+    print('확진율:',round(caseper,2),"%")
+    print('완치율:',data["recoveredPercentage"],"%")
+    print('사망률:',data["deathPercentage"],"%")
+
+    print("\n")
+    print('API Service by Goodbye-Corona(https://api.corona-19.kr)')
+    print('===========================================================')
+  else: #API 응답 헤더 코드가 정상(200)이 아닐 경우, Response Code와 Response Message 노출
+    print('=== [ ERROR REPORTING ] ===')
+    print("\n")
+    print('API Response Code(KOREA):',data["resultCode"])
+    print('API Response Message(KOREA):',data["resultMessage"])
+    print("\n")
+    print('API Response Code(COUNTRY):',data2["resultCode"])
+    print('API Response Message(COUNTRY):',data2["resultMessage"])
+    print("\n")
+    print('API Service by Goodbye-Corona(https://api.corona-19.kr)')
+    print('============================')
+else: #API 응답 헤더 코드가 정상(200)이 아닐 경우, Response Code와 Response Message 노출
+    print('=== [ ERROR REPORTING ] ===')
+    print("\n")
+    print('API Response Code(KOREA):',data["resultCode"])
+    print('API Response Message(KOREA):',data["resultMessage"])
+    print("\n")
+    print('API Response Code(COUNTRY):',data2["resultCode"])
+    print('API Response Message(COUNTRY):',data2["resultMessage"])
+    print("\n")
+    print('API Service by Goodbye-Corona(https://api.corona-19.kr)')
+    print('============================')
